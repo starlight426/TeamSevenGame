08c87e53426535d31b7c5179fee199f3b399fc95
starlight426 <147207252+starlight426@users.noreply.github.com>
2025-02-01 15:56:07 -0500

    more bullet stuff

--- Scripts/Combat/bullet_patterner.gd
+++ Scripts/Combat/bullet_patterner.gd
@@ -9,13 +9,14 @@ var dmg_to_spawn #damage of bullets
 var size_to_spawn #size modifier of bullets
 var team_to_spawn #team of bullets
 var color_to_spawn #color of bullets
-
+var delay_to_spawn #delay of bullets
+var anim_to_spawn #anim for bullets
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-func spawn_pattern(pattern,passed_type,passed_pos,passed_dir,passed_speed,passed_size,passed_dmg,passed_team,passed_color):
+func spawn_pattern(passed_pos,passed_dir,pattern,passed_pattern_scale,passed_delay,passed_anim,passed_type,passed_speed,passed_size,passed_dmg,passed_team,passed_color):
 	
 	#these variables are static and have to be edited between spawns of bullets if you want them to change
 	speed_to_spawn = passed_speed
@@ -23,11 +24,11 @@ func spawn_pattern(pattern,passed_type,passed_pos,passed_dir,passed_speed,passed
 	dmg_to_spawn = passed_dmg
 	team_to_spawn = passed_team
 	color_to_spawn = passed_color
+	delay_to_spawn = passed_delay
 	
 	#these variables are passed to the spawn function every time because they change a lot
 	var pos = passed_pos
 	var dir = passed_dir
-	
 	#burner variables
 	var boffset
 	var spacing
@@ -37,60 +38,65 @@ func spawn_pattern(pattern,passed_type,passed_pos,passed_dir,passed_speed,passed
 	match pattern:
 		
 		"single":
-			sp(pos,dir)
+			sp(pos,dir,1)
 			
 		"3cone":
-			sp(pos,dir)
-			sp(pos,dir+PI/6)
-			sp(pos,dir-PI/6)
+			sp(pos,dir,0)
+			sp(pos,dir+PI/6,1)
+			sp(pos,dir-PI/6,2)
 			
 		"6cone":
-			sp(pos,dir+PI/12)
-			sp(pos,dir-PI/12)
-			sp(pos,dir+PI/6)
-			sp(pos,dir-PI/6)
-			sp(pos,dir+PI/4)
-			sp(pos,dir-PI/4)
+			sp(pos,dir+PI/12,0)
+			sp(pos,dir-PI/12,0)
+			sp(pos,dir+PI/6,1)
+			sp(pos,dir-PI/6,1)
+			sp(pos,dir+PI/4,2)
+			sp(pos,dir-PI/4,2)
 			
 		"7cone":
-			sp(pos,dir)
-			sp(pos,dir+PI/6)
-			sp(pos,dir-PI/6)
-			sp(pos,dir+PI/3)
-			sp(pos,dir-PI/3)
-			sp(pos,dir+PI/2)
-			sp(pos,dir-PI/2)
+			sp(pos,dir,0)
+			sp(pos,dir+PI/6,1)
+			sp(pos,dir-PI/6,1)
+			sp(pos,dir+PI/3,2)
+			sp(pos,dir-PI/3,2)
+			sp(pos,dir+PI/2,3)
+			sp(pos,dir-PI/2,3)
 			
 		"5conetight":
-			sp(pos,dir)
-			sp(pos,dir+PI/6)
-			sp(pos,dir-PI/6)
-			sp(pos,dir+PI/12)
-			sp(pos,dir-PI/12)
+			sp(pos,dir,0)
+			sp(pos,dir+PI/6,1)
+			sp(pos,dir-PI/6,1)
+			sp(pos,dir+PI/12,2)
+			sp(pos,dir-PI/12,2)
 			
 		"3spread":
-			sp(pos,dir)
+			sp(pos,dir,0)
 			spacing = 600
 			off_angle = dir - (PI / 2)
 			boffset = Vector2(spacing * cos(off_angle), spacing * sin(off_angle))
-			sp(pos+boffset,dir)
+			sp(pos+boffset,dir,1)
 			off_angle = dir + (PI / 2)
 			boffset = Vector2(spacing * cos(off_angle), spacing * sin(off_angle))
-			sp(pos+boffset,dir)
+			sp(pos+boffset,dir,1)
 	
 		"2spread":
 			spacing = 300
 			off_angle = dir - (PI / 2)
 			boffset = Vector2(spacing * cos(off_angle), spacing * sin(off_angle))
-			sp(pos+boffset,dir)
+			sp(pos+boffset,dir,0)
 			off_angle = dir + (PI / 2)
 			boffset = Vector2(spacing * cos(off_angle), spacing * sin(off_angle))
-			sp(pos+boffset,dir)
+			sp(pos+boffset,dir,0)
 		
 	#delete self to reduce clutter
 	queue_free()
 
-func sp(pos,dir):
+func sp(pos,dir,delay_num):
+	if(delay_num*delay_to_spawn > 0):
+		await get_tree().create_timer(delay_num*delay_to_spawn).timeout
+	if(anim_to_spawn != "default"):
+		#anim code
+		pass
 	var bspawner = bullet_spawner.instantiate()
 	add_sibling(bspawner)
 	bspawner.shoot(btype,pos,dir,bspeed,bsize,bdmg,bteam,bcolor)
--- Scripts/Global/AttackSpawner.gd
+++ Scripts/Global/AttackSpawner.gd
@@ -7,11 +7,13 @@ func _ready() -> void:
 
 #This script contains global functions for spawning bullet and solid patterns. 
 
-#examples of calls:
-#spawn_bullets(global_position,global_rotation-PI/2,"3cone",1,"circle","arc","default",1.5,30,"player","purple",PI/6,0,0)
+#examples of calls (feel free to make more to copy paste):
+#spawn_bullets(global_position,global_rotation-PI/2,"3cone",1,0.1,"default",circle","arc","default",1.5,30,"player","purple",PI/6,0,0)
 
 #pattern: bullet pattern passed to the next node
 #pattern_scale: the size modifier of the pattern, input 1 for default
+#pattern_delay: the delay between each spawned bullet, input 0 for none
+#anim: animation that plays before each bullet spawn, input "default" for none
 #shape: shape of the bullet
 #type: movement pattern of bullet input "default" for default
 #speed: linear speed of bullet, input "default" for default
@@ -21,10 +23,9 @@ func _ready() -> void:
 #color: color of bullet (might remove idk)
 #mod1,mod2,mod3: extra arguments that may be used for the bullet movement. in an arc bullet, mod1 might contain the angle
 #hp (solid only) hp modifier. input "default" for default.
-
 #in solid spawner, the only difference is hp tacked on at the end, and mod1-3 being enemy subtypes.
 
-func spawn_bullets(pos,dir,pattern,pattern_scale,shape,type,speed,size,dmg,team,color,mod1,mod2,mod3):
+func spawn_bullets(pos,dir,pattern,pattern_scale,pattern_delay,anim,shape,type,speed,size,dmg,team,color,mod1,mod2,mod3):
 	var patterner = bullet_patterner_scn.instantiate()
 	add_sibling(patterner)
 	return patterner.spawn_pattern(pos,dir,pattern,pattern_scale,shape,type,speed,size,dmg,team,color,mod1,mod2,mod3)
