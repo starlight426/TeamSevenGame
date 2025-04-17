3c50041b58880e3802c1f4a11719a54b4396c69c
ShinyMudkip13 <bcolaiuta08@gmail.com>
2025-04-16 20:37:07 -0400

    Initial pass of Inventory Screen
    Created icon locations and labels for inventory, will update text and icons with the correct ones when added.

---
+++ UI Scenes/inventoryLabel.gd
@@ -0,0 +1,12 @@
+extends TextureRect
+
+func _ready():
+	connect("mouse_entered", Callable(self, "_on_mouse_entered"))
+	connect("mouse_exited", Callable(self, "_on_mouse_exited"))
+
+
+func _on_mouse_entered():
+	$Label.visible = true
+
+func _on_mouse_exited():
+	$Label.visible = false
